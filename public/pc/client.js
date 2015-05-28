f76eb80512358b227b9c9042c479a824c1fa46e3
DJ Nadgar <dj@exciteresearch.com>
2015-05-28 11:25:32 -0400

    pre heroku tests successful of semi finished web app

--- public/pc/client.js
+++ public/pc/client.js
@@ -51,8 +51,8 @@ pc.script.create('client', function (context) {
             var self = this;
             var servers = {
                 'local': 'http://localhost:30043/socket', // local
-                 // 'fsb': 'http://192.168.1.216:30043/socket', //fsb
-              'fsb': 'http://localhost:30043/socket', //fsbx
+                'fsb': 'http://192.168.1.216:30043/socket', //fsb
+                //'fsb': 'http://localhost:30043/socket', //fsbx
                 'us': 'http://54.67.22.188:30043/socket', // us
                 'default': 'https://tanx.playcanvas.com/socket' // load balanced
             };
